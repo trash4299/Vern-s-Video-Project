5de8f3f7ace97678fec08ad7207ee172cfffda1b
Vincent Lobo <vincelobo4299@gmail.com>
2016-08-30 13:04:07 -0500

    Add files via upload

---
+++ Admin .java
@@ -0,0 +1,25 @@
+
+//imports
+public class Admin
+{
+	//variables
+	private boolean firstTime;
+	
+	public Admin()
+	{}
+		//http://www.quarkphysics.ca/scripsi/java_notepad/
+		
+	}
+	
+	checkPassword()
+	{
+		
+	}
+	
+	setCreds()
+	{
+		
+	}
+	
+	
+}
