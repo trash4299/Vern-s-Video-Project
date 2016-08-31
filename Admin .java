90b9ffaed53b4605d50b226837f7d42a8c653c9e
Vincent Lobo <vincelobo4299@gmail.com>
2016-08-30 23:28:26 -0500

    Update Admin .java

--- Admin .java
+++ Admin .java
@@ -1,25 +1,51 @@
 
-//imports
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+import javax.swing.JFrame;
+
 public class Admin
 {
-	//variables
-	private boolean firstTime;
-	
-	public Admin()
-	{}
-		//http://www.quarkphysics.ca/scripsi/java_notepad/
-		
-	}
-	
-	checkPassword()
-	{
-		
-	}
-	
-	setCreds()
-	{
-		
-	}
-	
-	
+    private boolean firstTime;
+    private String password;
+    private String q;
+    private String s;
+   
+    public Admin()
+    {
+        firstTime=true;
+        s="";
+        q="";
+    }
+    
+    public void setCreds()
+    {
+        //remember to replace all /'s with double slashes
+        if(firstTime==false)
+        {
+            
+        }
+        else
+        {
+            JFrame frame = new JFrame();
+            s = JOptionPane.showInputDialog(frame,"Enter a new password:","Set Password",JOptionPane.PLAIN_MESSAGE);
+            q = JOptionPane.showInputDialog(frame,"Re-enter password:","Set Password",JOptionPane.PLAIN_MESSAGE);
+            if(s.equals(q)==false)
+            {
+                int n = JOptionPane.showConfirmDialog(frame, " Try new password or JOptionPane.YES_NO_OPTION","Passwords do not match" );
+                if()
+                while()
+            }
+        }
+    }
+    
+    public int checkCreds(String in)
+    {
+        if(firstTime==true)
+            return 0;
+        else if(password.equals(in))
+            return 1;
+        else
+            return -1;
+        
+    }
 }
